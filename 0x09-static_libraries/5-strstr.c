bb71559ea4e2bc2b38f643e3ca4759d02830216a
“marylishop” <marylishop@yahoo.com>
2023-05-13 02:43:46 -0700

    task 0

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -16,10 +16,10 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
-				{
-				if (haystack[i + j] != needle[j])
-					break;
-				}
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
 		if (!needle[j])
 			return (&haystack[i]);
 	}
