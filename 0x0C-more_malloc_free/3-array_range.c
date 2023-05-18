36c9d52a5739881667085362d434c3a7ca04bbee
“marylishop” <marylishop@yahoo.com>
2023-05-18 09:45:08 -0700

    add all the tasks

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,22 +13,20 @@
  */
 int *array_range(int min, int max)
 {
-	if (min < max)
+	int *arr;
+	int i;
+
+	if (min > max)
 	{
 		return (NULL);
 	}
-	int size = max - min + 1;
-	int *arr = malloc(size * sizeof(int));
+	arr = malloc(sizeof(*arr) * ((max - min) + 1));
 
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i <= max; i++, min++)
 		arr[i] = min + i;
-	}
 
 	return (arr);
 }
