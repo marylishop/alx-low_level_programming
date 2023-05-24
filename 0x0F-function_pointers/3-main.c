7517eb42ea39c69614b449a61cd62fe55d7e5398
“marylishop” <marylishop@yahoo.com>
2023-05-25 01:13:18 +0400

    added all tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
-# include "3-calc.h"
+#include "3-calc.h"
+
 /**
  * main - check the code for ALX students.
  * @argc: argument count.
@@ -8,23 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-int a, b;
-int (*operation)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
 }
-operation = get_op_func(argv[2]);
-if (operation == NULL)
-{
-printf("Error\n");
-exit(99);
-}
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-printf("%d\n", operator(a, b));
-return (0);
